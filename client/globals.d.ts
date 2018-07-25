9f66b38c5c679ee664f5b66ffea76bba0eca7062
Cedric Chee <cedric@invictusbyte.com>
2018-07-25 19:42:19 +0800

    Add globals definition file

--- client/globals.d.ts
+++ client/globals.d.ts
@@ -4,3 +4,39 @@ declare module 'bar' {
     foo: string;
   };
 }
+
+// declare module 'jquery';
+
+// External non-JS resources
+// declare module '*.css';
+
+// Ambient declarations
+// Declaration file
+// declare var foo: any;
+
+// Variables
+// For example to tell TypeScript about the process variable you can do:
+// declare var process: any;
+
+// declare namespace NodeJS {
+//   export interface Process extends EventEmitter {
+
+//   }
+
+//   export class EventEmitter {
+//   }
+// }
+// declare var process: NodeJS.Process;
+
+// interface Process {
+//   exit(code?: number): void;
+// }
+// declare var process: Process;
+
+
+// Modifying Native Types
+// Example window
+// Just add stuff to the Window interface e.g.:
+interface Window {
+  helloWorld(): void;
+}
