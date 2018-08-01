4012e843e30a82d52f0befbe74f2e540996f76ec
Cedric Chee <cedric@invictusbyte.com>
2018-08-01 21:53:48 +0800

    New chap for using Jest with TS for testing React components

---
+++ client/testing.ts
@@ -0,0 +1,85 @@
+// Testing
+
+// TypeScript can be used with any JavaScript testing framework that you want.
+// In the worst case you can always do a simple TypeScript -> JavaScript
+// transform and go your merry way.
+
+// That said, in this section look at options that we have enjoyed greatly 🌹
+
+// ----------------------------------------------------------------------------
+
+// Using Jest with TypeScript
+
+// No testing solution out there is perfect. That said, jest is an excellent
+// unit testing option which provides great TypeScript support.
+
+// > Note: We assume you start off with a simple node package.json setup. Also
+// > all TypeScript files should be in a src folder which is always recommended
+// > (even without Jest) for a clean project setup.
+
+// Step 1: Install
+
+// Install the following using npm:
+// $ npm i jest @types/jest ts-jest -D
+
+// Step 2: Configure Jest
+
+// Step 3: Run tests
+// Run npx jest from your project root and jest will execute any tests
+// you have.
+
+// Optional: Add script target for npm scripts
+
+// Optional: Run jest in watch mode
+
+// Example
+// See my typescript-testing repo
+
+// Example async
+// See my typescript-testing repo
+
+// Example enzyme
+// Enzyme allows you to test react components with dom support.
+// See my typescript-testing repo
+
+// Reasons why we like jest
+// For details on these features see jest website
+// - Built-in assertion library.
+// - Great TypeScript support.
+// - Very reliable test watcher.
+// - Snapshot testing.
+// - Built-in coverage reports.
+// - Built-in async/await support.
+
+// ----------------------------------------------------------------------------
+
+// Why Cypress
+// Cypress is a great E2E testing tool. Here are a few great reasons to
+// consider it:
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------------------------------
