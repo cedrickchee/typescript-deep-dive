c25f8642635c31d7efb77c85ac48364f3c8e47d3
Cedric Chee <cedric@invictusbyte.com>
2018-08-01 17:52:54 +0800

    New codes to try TypeScript JSX support

---
+++ client/JSX.ts
@@ -0,0 +1,21 @@
+// ##############################
+// JSX Support
+// ##############################
+
+// TypeScript supports JSX transpilation and code analysis. If you are unfamiliar with JSX here is an excerpt from the official website:
+
+// The motivation behind JSX is to allow users to write HTML like views in JavaScript so that you can:
+// - Have the view Type Checked by the same code that is going to check your JavaScript
+// - Have the view be aware of the context it is going to operate under (i.e. strengthen the controller-view connection in traditional MVC).
+// - Reuse JavaScript patterns for HTML maintenance e.g. Array.prototype.map, ?:, switch etc instead of creating new (and probably poorly typed) alternatives.
+
+// This decreases the chances of errors and increases the maintainability of your user interfaces. The main consumer of JSX at this point is ReactJS from facebook. This is the usage of JSX that we will discuss here.
+
+// ----------------------------------------------------------------------------
+
+// React JSX
+
+// Setup
+// Our browser quickstart already sets you up to develop react applications. Here are the key highlights.
+
+// ----------------------------------------------------------------------------
